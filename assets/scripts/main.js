bec1b92c6505b852db906613fac9800345265813
青栀 <fanzheng.fz@alibaba-inc.com>
2014-08-01 17:17:30 +0800

    用户页面&路由

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -9,15 +9,45 @@ var app = angular.module('app', [
 // 路由
 app.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/');
+
+  // 用户
   $stateProvider
-    .state('route1', {
-      url: '/route1',
-      templateUrl: 'partials/a.html'
+    .state('user', {
+      url: '/user',
+      templateUrl: 'partials/user/user.html'
     })
-    .state('route2', {
-      url: '/route2',
-      templateUrl: 'partials/b.html'
+    .state('user.search', {
+      url: '/search',
+      templateUrl: 'partials/user/search.html'
+    })
+    .state('user.map', {
+      url: '/map',
+      templateUrl: 'partials/user/map.html'
+    })
+    .state('user.buy', {
+      url: '/buy',
+      templateUrl: 'partials/user/buy.html'
     });
+
+  // 商户
+  $stateProvider
+    .state('busi', {
+      url: '/busi',
+      templateUrl: 'partials/buis/buis.html'
+    })
+    .state('buis.dashboard', {
+      url: '/dashboard',
+      templateUrl: 'partials/busi/dashboard.html'
+    })
+    .state('buis.dashboard.order', {
+      url: '/order',
+      templateUrl: 'partials/busi/dashboard-order.html'
+    })
+    .state('buis.dashboard.store', {
+      url: '/store',
+      templateUrl: 'partials/busi/dashboard-store.html'
+    });
+
 })
 
 // 初始化运行时设置
