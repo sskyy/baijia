36d13acb39e9fb077b97d8edf8f9b0850f6fd79b
sskyy <jasononcourt@gmail.com>
2014-08-01 09:33:57 +0800

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-var http = require('http');
-var port = 18080;
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>Node.js</h1>');
-    res.end('<p>Hello World</p>');
-}).listen(port);
+var argv = require('optimist').argv
+
+argv.port = 18080
+
+require('sails').lift();
