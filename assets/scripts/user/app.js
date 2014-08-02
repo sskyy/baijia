69dca078bdcf14fb70e1c87e9ce212b09b7e1b13
青栀 <fanzheng.fz@alibaba-inc.com>
2014-08-02 16:15:45 +0800

    update

--- assets/scripts/user/app.js
+++ assets/scripts/user/app.js
@@ -10,7 +10,7 @@ var app = angular.module('app', [
 
 // 路由
 app.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/user');
+  $urlRouterProvider.otherwise('/login');
 
   // 用户
   $stateProvider
