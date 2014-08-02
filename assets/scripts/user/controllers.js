a3453bb58362a73d56c7415af6595e794099bdd1
yongbo <yongo.hello@gmail.com>
2014-08-02 12:33:15 +0800

    update

--- assets/scripts/user/controllers.js
+++ assets/scripts/user/controllers.js
@@ -25,19 +25,9 @@ app.controller('searchController', function($scope, $state) {
   };
 });
 
-app.controller('mapController', function($scope) {
-  var map = new BMap.Map("allmap");
-  map.centerAndZoom(new BMap.Point(31.201, 121.581), 14);
-
-  map.addControl(new BMap.ZoomControl());          //添加地图缩放控件
-
-  var marker1 = new BMap.Marker(new BMap.Point(31.201, 121.581));  // 创建标注
-
-  map.addOverlay(marker1);              // 将标注添加到地图中
-
-  //创建信息窗口
-  var infoWindow1 = new BMap.InfoWindow("普通标注");
-  marker1.addEventListener("click", function(){this.openInfoWindow(infoWindow1);});
+app.controller('mapController', function($scope, mapService) {
+  mapService.initMap();
+  console.log('map start');
 });
 
 app.controller('buyController', function($scope) {
--- assets/scripts/user/services.js
+++ assets/scripts/user/services.js
@@ -23,3 +23,61 @@ app.factory('dataService', function($http, $q) {
       getxxdata: getxxdata
     };
   });
+
+/* 百度map接口说明：
+ * map.Point(lng:Number, lat:Number)
+ *
+ */
+
+app.factory('mapService', function($http, $q) {
+
+
+
+  function getCustomPoints(map){
+    /*
+     * {  }
+     */
+    var points = [];
+    var bounds = map.getBounds();
+    var sw = bounds.getSouthWest();
+    var ne = bounds.getNorthEast();
+    var lngSpan = Math.abs(sw.lng - ne.lng);
+    var latSpan = Math.abs(ne.lat - sw.lat);
+
+    for (var i = 0; i < 15; i ++) {
+      var point = new BMap.Point(sw.lng + lngSpan * (Math.random() * 0.7), ne.lat - latSpan * (Math.random() * 0.7));
+      points.push(point);
+    }
+    return points;
+  }
+
+  function createPointsOnMap(map){
+    var points = getCustomPoints(map); // 获取随机坐标，用来创建点数据
+    for(var i=0;i<points.length;i++){
+      var marker = new BMap.Marker(points[i]);  // 创建标注
+      map.addOverlay(marker);// 将标注添加到地图中
+      //创建信息窗口
+      (function(i){
+        var infoWindow = new BMap.InfoWindow('<div>拉拉拉拉'+i+'</div>');
+        marker.addEventListener("click", function(){this.openInfoWindow(infoWindow);});
+      })(i);
+    }
+
+  }
+
+  function initMap() {
+    var me_point = new BMap.Point(121.581, 31.201); //用户当前位置坐标点
+    var map = new BMap.Map("J_mymap");
+    map.centerAndZoom(me_point, 11);
+    map.addControl(new BMap.ZoomControl()); //添加地图缩放控件
+    createPointsOnMap(map);
+    // var local = new BMap.LocalSearch(map, {
+    //   renderOptions:{map: map}
+    // });
+    // local.search("景点");
+  }
+
+  return {
+    initMap: initMap
+  };
+});
