9ab4dcb8f4785ed67026d4f1c44f713899185ba1
青栀 <fanzheng.fz@alibaba-inc.com>
2014-08-02 09:21:04 +0800

    补充map&buy controller

--- assets/scripts/user/controllers.js
+++ assets/scripts/user/controllers.js
@@ -6,13 +6,29 @@ var app = angular.module('app');
   // 登陆逻辑
 app.controller('loginController', function($scope, $state) {
   $scope.login = function() {
-    alert('login');
 
     // success
     if (true) {
       $state.go('search');
     } else {
-
+      // failure
     }
   };
+});
+
+app.controller('searchController', function($scope, $state) {
+  $scope.search = function() {
+
+    if (true) {
+      $state.go('map');
+    }
+  };
+});
+
+app.controller('mapController', function($scope) {
+  console.log('map');
+});
+
+app.controller('buyController', function($scope) {
+  console.log('buyController');
 });
