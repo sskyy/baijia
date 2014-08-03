72eff353b5fdde2832f4966efc839d9edd1aaa61
rainer_H <jasononcourt@gmail.com>
2014-08-03 11:41:30 +0800

    add location for orders

--- data/orders.js
+++ data/orders.js
@@ -4,7 +4,9 @@
 
 var _  = require('lodash')
 var userNames = ['小新','白白','加缪','达尔文','诺贝尔','济滇','青栀','桐杰','夜末','子溯','宫煌'],
-assetsNames = ['娃哈哈矿泉水','鲜橙多','杜蕾斯','香蕉片','鲜奶片','星巴克咖啡外卖版']
+    assetsNames = ['娃哈哈矿泉水','鲜橙多','杜蕾斯','香蕉片','鲜奶片','星巴克咖啡外卖版'],
+    lng = 121.5809783,
+    lat = 31.2019986
 
 var data = []
 
@@ -15,6 +17,10 @@ _.each( assetsNames, function(assetName ){
         user : {
             name : userNames[Math.floor( userNames.length * Math.random())],
             avatar : "/images/avatar_" + Math.floor( 10 * Math.random()) + ".png"
+        },
+        points : {
+            lng : lng + 0.1*Math.random(),
+            lat : lat + 0.1*Math.random()
         }
     }
     data.push(require)
